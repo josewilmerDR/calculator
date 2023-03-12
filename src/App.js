4ebc3cdc026fa6272483cff668d53e6fe01f479f
josewilmerdr <josewdr@gmail.com>
2023-03-12 06:38:43 -0600

    show you how to create a basic calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Boton from './componentes/Boton.jsx';
+import Pantalla from './componentes/Pantalla.jsx';
+import freCodeCampLogo from './img/fcc_primary_large.jpg'
+import BotonClear from './componentes/BotonClear';
+import React, {useState, useEffect} from 'react';
+import { evaluate } from 'mathjs';
 function App() {
+  const [input, setInput] = useState('')
+
+  const agregarInput = val =>{
+    setInput(input + val);
+  }
+
+  const limpiarPantalla  = () => {
+    setInput('');
+  }
+
+  const calcularResultado = () =>{
+    if(input){
+      setInput(evaluate(input))
+    }else{
+      alert("Por favor ingresa un valor para calcular.")
+    }
+  }
+
+  useEffect(()=>{},[input])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={freCodeCampLogo} alt='lodo de freeCodeCamp'/>
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+        <BotonClear limpiarPantalla={limpiarPantalla}>Clear</BotonClear>
+        </div>
+      </div>  
     </div>
   );
 }
 
-export default App;
+ export default App;
